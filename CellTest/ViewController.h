3a8955b012c697d9f27f0c86f89a0924d1b82425
Carlos Pages <carlos@incuna.com>
2015-03-18 10:12:11 +0000

    Initial Commit

---
+++ CellTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CellTest
+//
+//  Created by Carlos Pages on 18/03/2015.
+//  Copyright (c) 2015 IncunaLTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CellTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CellTest
+//
+//  Created by Carlos Pages on 18/03/2015.
+//  Copyright (c) 2015 IncunaLTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
